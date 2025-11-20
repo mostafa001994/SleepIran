39f8befbfbeb7cb2aa525f5add1ea2d5dd166723
Mostafa M <mosy001994@gmail.com>
2025-11-20 08:26:21 +0330

    product single

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,7 +1,7 @@
 
 
-var thumbs_movie = new Swiper(
-    ".thumbs_movie", {
+var thumbs_image_gallery = new Swiper(
+    ".thumbs_image_gallery", {
     spaceBetween: 10,
     slidesPerView: 4,
     // freeMode: true,
@@ -16,24 +16,21 @@ var thumbs_movie = new Swiper(
         },
 
         320: {
-            slidesPerView: 5,
-            direction: "vertical",
+            slidesPerView: 4,
             spaceBetween: 10,
 
 
         },
 
         480: {
-            slidesPerView: 5,
-            direction: "vertical",
+            slidesPerView: 4,
             spaceBetween: 10,
 
 
         },
 
         640: {
-            slidesPerView: 5,
-            direction: "vertical"
+            slidesPerView: 4,
 
         },
 
@@ -56,16 +53,23 @@ var thumbs_movie = new Swiper(
 });
 
 
-var slider_movie = new Swiper(
+var slider_image_gallery = new Swiper(
 
-    ".slider_movie", {
+    ".slider_image_gallery", {
     spaceBetween: 10,
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+
+    },
     thumbs: {
-        swiper: thumbs_movie,
+        swiper: thumbs_image_gallery,
     },
 });
 
@@ -73,14 +77,85 @@ var slider_movie = new Swiper(
 
 
 
+var thumbs_image_gallery2 = new Swiper(
+    ".thumbs_image_gallery2", {
+    spaceBetween: 10,
+    slidesPerView: 6,
+    // freeMode: true,
+    // watchSlidesProgress: true,
+
+    breakpoints: {
+        // when window width is >= 320px
+
+        300: {
+            // slidesPerView: 2,
+
+        },
+
+        320: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+
+
+        },
+
+        480: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+
+
+        },
+
+        640: {
+            slidesPerView: 4,
+
+        },
+
+        768: {
+            slidesPerView: 6,
+            direction: "horizontal"
+        },
+        1024: {
+            slidesPerView: 6,
+            direction: "horizontal"
+
+
+        },
+        1265: {
+            slidesPerView: 6,
+            direction: "horizontal"
+        }
+
+    },
+});
+
+
+var slider_image_gallery2 = new Swiper(
+
+    ".slider_image_gallery2", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: thumbs_image_gallery2,
+    },
+});
+
+
+
+
 
 const products = new Swiper(
     ".products", {
-    spaceBetween: 30,
+    spaceBetween: 10
+    ,
 
     loop: true,
 
     // centeredSlides:true,
+    slidesPerView: 4,
 
     // autoplay: true,
     // using "ratio" endpoints
@@ -89,21 +164,21 @@ const products = new Swiper(
         // when window width is >= 320px
 
         300: {
-            slidesPerView: 2,
+            slidesPerView: 1,
 
         },
 
         320: {
-            slidesPerView: 2,
+            slidesPerView: 1,
         },
 
         480: {
-            slidesPerView: 2,
+            slidesPerView: 1,
 
         },
 
         640: {
-            slidesPerView: 3,
+            slidesPerView: 2,
             // spaceBetween: 20,
         },
         768: {
@@ -125,6 +200,55 @@ const products = new Swiper(
 
     },
     // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+}
+)
+
+
+
+const testimonials = new Swiper(
+    ".testimonials", {
+    spaceBetween: 15
+    ,
+
+    loop: true,
+
+    // centeredSlides:true,
+
+    // autoplay: true,
+    // using "ratio" endpoints
+
+    breakpoints: {
+        // when window width is >= 320px
+
+
+        640: {
+            slidesPerView: 1,
+            // spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 1,
+
+        },
+        1265: {
+            slidesPerView: 2,
+        }
+
+
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+
+    },
+    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -135,46 +259,93 @@ const products = new Swiper(
 
 
 
+
+
 // 
-  function increaseValue() {
+function increaseValue() {
     let input = document.getElementById('quantity');
     input.value = parseInt(input.value) + 1;
-  }
+}
 
-  function decreaseValue() {
+function decreaseValue() {
     let input = document.getElementById('quantity');
     let current = parseInt(input.value);
     if (current > 1) {
-      input.value = current - 1;
+        input.value = current - 1;
     }
-  }
+}
 
 //   
 
-    window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const box = document.getElementById('myBox');
     if (window.scrollY > 600) { // وقتی اسکرول بیشتر از 300px شد
-      box.classList.add('fixed','w-11/12'  ,'p-2!' , 'shadow');
+        box.classList.add('fixed');
     } else {
-      box.classList.remove('fixed','w-11/12' ,'p-2!' , 'shadow');
+        box.classList.remove('fixed');
     }
-  });
+});
 
 
 //
 
 
-  const tabs = document.querySelectorAll('.tab-btn');
-  const contents = document.querySelectorAll('.tab-content');
+const tabs = document.querySelectorAll('.tab-btn');
+const contents = document.querySelectorAll('.tab-content');
 
-  tabs.forEach((tab, index) => {
+tabs.forEach((tab, index) => {
     tab.addEventListener('click', () => {
-      tabs.forEach(t => t.classList.remove('active'));
-      contents.forEach(c => c.classList.add('hidden'));
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(c => c.classList.add('hidden'));
 
-      tab.classList.add('active');
-      contents[index].classList.remove('hidden');
+        tab.classList.add('active');
+        contents[index].classList.remove('hidden');
     });
-  });
+});
+
+// 
+
+
+
+// 
+
+const accordionBtns = document.querySelectorAll(".accordion-btn");
+
+accordionBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        // همه آیتم‌ها بسته بشن
+        document.querySelectorAll(".accordion-content").forEach(c => c.classList.add("hidden"));
+        document.querySelectorAll(".accordion-btn .icon").forEach(i => i.classList.remove("rotate-180"));
+
+        // آیتم کلیک‌شده باز بشه
+        const content = btn.nextElementSibling;
+        const icon = btn.querySelector(".icon");
+        content.classList.remove("hidden");
+        icon.classList.add("rotate-180"); // چرخش آیکون
+    });
+});
+
+
+
+// 
+
+// باز کردن مودال
+document.querySelectorAll(".open-modal").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const targetId = btn.getAttribute("data-target");
+        const modal = document.getElementById(targetId);
+        modal.classList.remove("hidden");
+        modal.classList.add("flex");
+    });
+});
+
+// بستن مودال
+document.querySelectorAll(".close-modal").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const modal = btn.closest(".modal");
+        modal.classList.add("hidden");
+        modal.classList.remove("flex");
+    });
+});
 
 // 
